966488cca471149133cdeef3f74afcf4c3257b20
Ahmad <ahmadfarouk799@gmail.com>
2023-10-08 12:58:59 +0100

    hot-collections

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -26,6 +26,22 @@ const HotCollections = (props) => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
